fbadc15c8c95c26a0a7fbbd0b6f861cff4bb115c
Антон <antonvolynskii@gmail.com>
2020-04-16 23:25:15 +1000

    created array of different screen and added drawn buttons and keyboard buttons to toggle them

--- js/main.js
+++ js/main.js
@@ -1,15 +1,101 @@
-(function () {
 
-  const greeting = document.getElementById(`greeting`).content.querySelector(`.greeting.central--blur`);
+const mainScreen = document.querySelector(`.central`);
+  const greeting = document.getElementById(`greeting`);
+  const rules = document.getElementById(`rules`);
+  const gameOne = document.getElementById(`game-1`);
+  const gameTwo = document.getElementById(`game-2`);
+  const gameThree = document.getElementById(`game-3`);
+  const stats = document.getElementById(`stats`);
   if (greeting) {
     console.log(greeting);
   }
+  displayArray = [mainScreen,greeting,rules,gameOne,gameTwo,gameThree,stats];
+console.log(displayArray);
 
-  const rules = document.getElementById(`rules`);
-  const rulesHeader = rules.content.querySelector(`.header`);
-  const rulesMain = rules.content.querySelector(`.rules`);
-  const rulesFooter = rules.content.querySelector(`.footer`);
+  var sliderControls = document.querySelectorAll(".arrows__btn");
+console.log(sliderControls);
+// slides informations
+  var slides = displayArray;
+    slidesLength = slides.length;
 
-  const game1 = document.getElementById(`game-1`).content;
-  console.log(rulesHeader);
-})();
+// slides array
+  var slidesArr = [].slice.call(slides);
+// reverse array sorting
+  slidesArr = slidesArr.reverse();
+
+ // slide current
+  var slideCurrent = 0;
+  for (var i = 0; i < sliderControls.length; i++)
+    sliderControls[i].addEventListener("click", function(e) {
+      target = e.target;
+
+      // get next button
+      if(target.classList.contains("next")) {
+        next = e.target,
+          prev = next.previousElementSibling,
+          nextSlide = slidesArr[slideCurrent + 1],
+          slide = slidesArr[slideCurrent];
+        slideCurrent += 1;
+
+        if(slideCurrent > 0) {
+          prev.disabled = false;
+        }
+        if(slideCurrent === slidesLength - 1){
+          next.disabled = true;
+        }
+      }
+      // get prev button
+      if(target.classList.contains("prev")){
+        slideCurrent -= 1;
+
+        prev = e.target,
+          next = prev.nextElementSibling,
+          prevSlide = slidesArr[slideCurrent + 1],
+          slide = slidesArr[slideCurrent];
+
+        if(slideCurrent === slidesLength - 2){
+          next.disabled = false;
+        }
+
+        if(slideCurrent === 0){
+          prev.disabled = true;
+        }
+      }
+      console.log(slides[slideCurrent])
+      mainScreen.innerHTML = slides[slideCurrent].innerHTML;
+});
+  var arrowLeftKeyCode = 37;
+var arrowRightKeyCode = 39;
+document.addEventListener("keydown", function (e) {
+ if(e.keyCode === arrowRightKeyCode) {
+   next = e.target,
+     prev = next.previousElementSibling,
+     nextSlide = slidesArr[slideCurrent + 1],
+     slide = slidesArr[slideCurrent];
+   slideCurrent += 1;
+
+   if(slideCurrent > 0) {
+     prev.disabled = false;
+   }
+   if(slideCurrent === slidesLength - 1){
+     next.disabled = true;
+   }
+ };
+  if(e.keyCode === arrowLeftKeyCode) {
+    slideCurrent -= 1;
+
+    prev = e.target,
+      next = prev.nextElementSibling,
+      prevSlide = slidesArr[slideCurrent + 1],
+      slide = slidesArr[slideCurrent];
+
+    if(slideCurrent === slidesLength - 2){
+      //need to disable slider somehow
+    }
+
+    if(slideCurrent === 0){
+      prev.disabled = true;
+    }
+  };
+  mainScreen.innerHTML = slides[slideCurrent].innerHTML;
+});
